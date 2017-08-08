f41baf1f0232f6c68649a7d8cee4a34c34ecf9dc
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-08 09:44:26 +0700

    Users Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import usersReducers from './users';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  users: usersReducers
 });
 
 export default rootReducer;
